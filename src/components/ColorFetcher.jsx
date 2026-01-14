0f51b8f82e022b27864d808d1037dc8d1370be00
ClaudeDebussy <15807600+ClaudeDebussy@users.noreply.github.com>
2026-01-14 12:35:51 +0500

    implement fetch multiple colors

--- src/components/ColorFetcher.jsx
+++ src/components/ColorFetcher.jsx
@@ -1,77 +1,82 @@
 import { useEffect, useState } from "react";
 
-const ColorFetcher = () => {
-  // Helper functions:
-  const getRandomHsl = () => {
-    // settings below for vibrant colors
-    const satMax = 90;
-    const satMin = 50;
-    const lightMax = 60;
-    const lightMin = 40;
+const getRandomHsl = () => {
+  // settings below for vibrant colors
+  const satMax = 90;
+  const satMin = 50;
+  const lightMax = 60;
+  const lightMin = 40;
 
-    const hue = Math.floor(Math.random() * 360);
-    const saturation = Math.floor(Math.random() * (satMax - satMin) + satMin);
-    const lightness = Math.floor(
-      Math.random() * (lightMax - lightMin) + lightMin
+  const hue = Math.floor(Math.random() * 360);
+  const saturation = Math.floor(Math.random() * (satMax - satMin) + satMin);
+  const lightness = Math.floor(
+    Math.random() * (lightMax - lightMin) + lightMin
+  );
+
+  return {
+    hue,
+    saturation,
+    lightness,
+  };
+};
+
+const getArrayOfHslValues = () => {
+  const desiredNumberOfColors = 12;
+  const array = [];
+
+  function addNewHslToArray(arr) {
+    const newHsl = getRandomHsl();
+    arr.push(newHsl);
+    // TODO: check if color already exists
+  }
+
+  while (array.length < desiredNumberOfColors) {
+    addNewHslToArray(array);
+  }
+
+  return array;
+};
+
+const getArrayOfUrls = (colors) => {
+  const arr = [];
+  for (let i = 0; i < colors.length; i++) {
+    arr.push(
+      `https://www.thecolorapi.com/id?hsl=${colors[i].hue},${colors[i].saturation}%,${colors[i].lightness}%`
     );
+  }
+  return arr;
+};
 
-    return {
-      hue,
-      saturation,
-      lightness,
-    };
-  };
-
-  const getArrayOfHslValues = () => {
-    const desiredNumberOfColors = 12;
-    const array = [];
-
-    function addNewHslToArray(arr) {
-      const newHsl = getRandomHsl();
-      //check if color already exists
-      if (!arr.includes(newHsl)) {
-        arr.push(newHsl);
-      }
-    }
-
-    while (array.length < desiredNumberOfColors) {
-      addNewHslToArray(array);
-    }
-
-    return array;
-  };
-
-  const getArrayOfUrls = (colors) => {
-    const arr = [];
-    for (let i = 0; i < colors.length; i++) {
-      arr.push(
-        `https://www.thecolorapi.com/id?hsl=${colors[i].hue},${colors[i].saturation}%,${colors[i].lightness}%`
-      );
-    }
-    return arr;
-  };
-
-  const hslValues = getArrayOfHslValues();
-  const urls = getArrayOfUrls(hslValues);
-
-  const randomHslValues = getRandomHsl();
-  const url = `https://www.thecolorapi.com/id?hsl=${randomHslValues.hue},${randomHslValues.saturation}%,${randomHslValues.lightness}%`;
-
-  // Component:
+const ColorFetcher = () => {
   const [data, setData] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    const fetchOneColor = async () => {
+    const hslValues = getArrayOfHslValues();
+    const urls = getArrayOfUrls(hslValues);
+
+    const fetchMultipleColors = async () => {
       try {
-        const response = await fetch(url);
-        if (!response.ok) {
-          throw new Error(`HTTP error! Status ${response.status}`);
+        const urlsToFetch = urls;
+
+        const responses = await Promise.all(
+          urlsToFetch.map((url) => fetch(url))
+        );
+
+        for (const response of responses) {
+          if (!response.ok) {
+            throw new Error(`HTTP error! Status ${response.status}`);
+          }
         }
-        const result = await response.json();
-        setData(result);
-        setError(null);
+
+        const jsonDatas = await Promise.all(
+          responses.map((response) => response.json())
+        );
+
+        await setData(jsonDatas);
+
+        setIsLoading(false);
       } catch (err) {
         setError(err.message);
         setData(null);
@@ -80,7 +85,7 @@ const ColorFetcher = () => {
       }
     };
 
-    fetchOneColor();
+    fetchMultipleColors();
   }, []);
 
   if (isLoading) {
@@ -93,7 +98,9 @@ const ColorFetcher = () => {
 
   return (
     <div>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
+      {data.map((color) => (
+        <pre key={color.hex.value}>{JSON.stringify(color, null, 2)}</pre>
+      ))}
     </div>
   );
 };
