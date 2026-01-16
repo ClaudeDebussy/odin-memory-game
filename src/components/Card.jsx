1d012ba224adca8971645afe6ef0b0214688e83a
ClaudeDebussy <15807600+ClaudeDebussy@users.noreply.github.com>
2026-01-16 13:03:57 +0500

    implement hover effect over Cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,36 @@
+import { useState } from "react";
 import "../styles/card.css";
 
 function Card(props) {
-  const colorStyle = {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const baseStyle = {
     backgroundColor: `hsl(${props.color.h},${props.color.s}%,${props.color.l}%)`,
     color: `${props.color.contrastHex}`,
+    transition: "background-color 0.1s ease, transform 0.1s ease",
+  };
+
+  const hoveredStyle = {
+    backgroundColor: `hsl(${props.color.h},${props.color.s}%,${
+      props.color.l - 10
+    }%)`,
+    color: `${props.color.contrastHex}`,
+    transform: "rotate(2deg)",
+  };
+
+  const currentStyle = {
+    ...baseStyle,
+    ...(isHovered && hoveredStyle),
   };
 
   return (
-    <div className="card" style={colorStyle} onClick={props.shuffle}>
+    <div
+      className="card"
+      style={currentStyle}
+      onClick={props.shuffle}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
       <h1>{props.color.name}</h1>
     </div>
   );
